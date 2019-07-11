870068f61bbeff3fb491f57a61e9cd385760cd2b
Oleksii Pelekh <oleksii.pelekh@symphony-solutions.eu>
2019-07-11 17:56:02 +0300

    Added handler with no functionality for the test purposes.

--- HYParentalGate/HYParentalGate.swift
+++ HYParentalGate/HYParentalGate.swift
@@ -88,8 +88,18 @@ public class HYParentalGate: NSObject, HYParentalGateViewDelegate, UIGestureReco
         overlay?.backgroundColor = UIColor(white: 0.4, alpha: 0.6)
         overlay?.tag = overlayTag
         
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(HYParentalGate.handleTapOutsideDialog))
+        tapRecognizer.delegate = self
+        overlay?.addGestureRecognizer(tapRecognizer)
+
         window.addSubview(overlay!)
     }
+    
+    @objc fileprivate func handleTapOutsideDialog() {
+//        cancelHandler()
+//        hide()
+    }
+
     
     fileprivate func calculateParentalGateFrame() -> CGRect {
         let widthCoeff: CGFloat
